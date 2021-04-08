a16a2b1ae7bf0345e17557ead9e69e21a95aba4d
Anton Vetrov <Antosya.net@mail.ru>
2021-04-08 20:09:22 +0300

    init gradle project

---
+++ online_store_api/src/main/java/org/online/store/api/OnlineStoreApiApplication.java
@@ -0,0 +1,11 @@
+package org.online.store.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreApiApplication.class, args);
+    }
+}
